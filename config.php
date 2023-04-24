af2a41a60f79e5788e27e6b0e375cf28a02b5b78
fullwebdevs <jimmydream917@gmail.com>
2023-04-23 20:12:46 -0500

    testing dev

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     $SECRET_KEY = "8ca2fd2b6bc8d3451088c5432b3e00bc";
     $LocationID = "81148838197";
     $ShopHostName = "amconsultingdevstore.myshopify.com";
-    $APP_URL = "http://3.87.192.115/Externalmarketplace-shopifyapp";
+    $APP_URL = "http://3.87.192.115";
 
     $db_host = 'localhost';
     $db_user = 'root';
