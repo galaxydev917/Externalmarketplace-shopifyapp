f3622329d02f765a9ca54f7d92da8df425122ff2
fullwebdevs <jimmydream917@gmail.com>
2023-04-23 20:16:31 -0500

    testing

--- generate_token.php
+++ generate_token.php
@@ -2,31 +2,32 @@
     require 'config.php';
     header('Access-Control-Allow-Origin: *');
     $params = $_GET; 
-    $hmac = $_GET['hmac'];
-    $params = array_diff_key($params, array('hmac' => ''));
-    ksort($params); 
-    $computed_hmac = hash_hmac('sha256', http_build_query($params), $SECRET_KEY);
+    print_r($params);
+    // $hmac = $_GET['hmac'];
+    // $params = array_diff_key($params, array('hmac' => ''));
+    // ksort($params); 
+    // $computed_hmac = hash_hmac('sha256', http_build_query($params), $SECRET_KEY);
 
-    // Use hmac data to check that the response is from Shopify or not
-    if (hash_equals($hmac, $computed_hmac)) {
-        $query = array(
-            "client_id" => $API_KEY, 
-            "client_secret" => $SECRET_KEY, 
-            "code" => $params['code']
-        );
-        // Generate access token URL
-        $access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+    // // Use hmac data to check that the response is from Shopify or not
+    // if (hash_equals($hmac, $computed_hmac)) {
+    //     $query = array(
+    //         "client_id" => $API_KEY, 
+    //         "client_secret" => $SECRET_KEY, 
+    //         "code" => $params['code']
+    //     );
+    //     // Generate access token URL
+    //     $access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
 
-        // Configure curl client and execute request
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_URL, $access_token_url);
-        curl_setopt($ch, CURLOPT_POST, count($query));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $data = json_decode($result, true);
-        print_r($data);
+    //     // Configure curl client and execute request
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //     curl_setopt($ch, CURLOPT_URL, $access_token_url);
+    //     curl_setopt($ch, CURLOPT_POST, count($query));
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+    //     $result = curl_exec($ch);
+    //     curl_close($ch);
+    //     $data = json_decode($result, true);
+    //     print_r($data);
         // $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
         // if (!$conn) {
         //     die("Connection failed: " . mysqli_connect_error());
