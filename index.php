63bc95fe195c7b1d43cf5e0393ecf9d6d01e7c4b
fullwebdevs <jimmydream917@gmail.com>
2023-04-23 20:19:09 -0500

    test

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
   require 'config.php';
-  session_start();
-  if (isset($_GET['shop'])) {
-    $shop = $_GET['shop'];
-    $_SESSION['shop'] = $shop;
-    $scopes = "read_orders,write_products";
-    $redirect_uri = $APP_URL."/generate_token.php";
-    $install_url = "https://". $shop."/admin/oauth/authorize?client_id=".$API_KEY."&scope=".$scopes ."&secret=" . $SECRET_KEY . "&redirect_uri=" . urlencode($redirect_uri);
-    header("Location: " . $install_url);
-    die();
-  }
+echo "aaaaaaaa";
+  // if (isset($_GET['shop'])) {
+  //   $shop = $_GET['shop'];
+  //   $_SESSION['shop'] = $shop;
+  //   $scopes = "read_orders,write_products";
+  //   $redirect_uri = $APP_URL."/generate_token.php";
+  //   $install_url = "https://". $shop."/admin/oauth/authorize?client_id=".$API_KEY."&scope=".$scopes ."&secret=" . $SECRET_KEY . "&redirect_uri=" . urlencode($redirect_uri);
+  //   header("Location: " . $install_url);
+  //   die();
+  // }
 ?>
